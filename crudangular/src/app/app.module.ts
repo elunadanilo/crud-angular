ef5aa5130568764ede47c18fa4dd665e6a99c319
elunadanilo <eluna.danilo@gmail.com>
2021-08-20 23:14:54 -0600

    formato
    Se agrega formato a formulario

--- crudangular/src/app/app.module.ts
+++ crudangular/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
     TarjetaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- crudangular/src/app/components/tarjeta/tarjeta.component.ts
+++ crudangular/src/app/components/tarjeta/tarjeta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder ,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta',
@@ -13,9 +14,22 @@ export class TarjetaComponent implements OnInit {
     {titulo: "Pedro Perez", numerotarjeta: "123456789012", fechaExpiracion: "21/12/2023", cvv: "121"}
   ];
 
-  constructor() { }
+  form: FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titulo: [''],
+      numeroTarjeta: [''],
+      fechaExpiracion: [''],
+      cvv: ['']
+    })
+   }
 
   ngOnInit(): void {
   }
 
+  agregarTarjeta(){
+    console.log(this.form);
+  }
 }
